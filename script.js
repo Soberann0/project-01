9243f814062e36f767399e4e697fafbd9742cd15
Nick <iorranysnew@hotmail.com>
2023-03-09 10:00:38 -0300

    layout 90 por cento

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ menubar.onclick = () =>{
     menubar.classList.toggle('fa-times')
     nav.classList.toggle('active')
 }
+
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+  })
+
+  sr.reveal('.myimageabout',{ delay:350, origin:'left' })
+  sr.reveal('.about-text-content',{ delay:350, origin:'right' })
+  sr.reveal('.home-image',{ delay:350, origin:'left' })
+  sr.reveal('.home-text-content',{ delay:350, origin:'right' })
+
+  sr.reveal('.appetizer-content',{ delay:350, origin:'left' })
+  sr.reveal('.appetizer-image',{ delay:350, origin:'right' })
+  sr.reveal('.gallery-image',{ delay:350, origin:'top' })
+  sr.reveal('.menu-food-content',{ delay:350, origin:'top' })
+  sr.reveal('.menu-food-text',{ delay:350, origin:'bottom' })
+  sr.reveal('.food-main-content',{ delay:350, origin:'bottom' })
+  sr.reveal('.before',{ delay:350, origin:'bottom' })
+  sr.reveal('.footer-logo',{ delay:350, origin:'bottom' })
+
+
